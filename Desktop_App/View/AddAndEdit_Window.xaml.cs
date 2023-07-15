8788fd98df7b6e2c91e01c28a3685b9b84c69b4e
Mahesh_Wijerathna <Mahesh_Wijerathna@MAHESH-DESKTOP>
2023-07-15 13:44:25 +0530

    Add project files.

---
+++ Desktop_App/View/AddAndEdit_Window.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Desktop_App.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop_App.View
+{
+    /// <summary>
+    /// Interaction logic for AddAndEdit_Window.xaml
+    /// </summary>
+    public partial class AddAndEdit_Window : Window
+    {
+        public AddAndEdit_Window(AddAndEditWIndow_VM vm_datacontext)
+        {
+            InitializeComponent();
+            DataContext = vm_datacontext;
+            
+        }
+    }
+}
